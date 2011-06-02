0819c2c427b4358b53274ebcf7defe2f5efb1eb9
David Patt <david.m.patt@gmail.com>
2011-06-02 00:03:13 -0700

    Done with demo final

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.gem "mongo_mapper"
+
 # Initialize the rails application
 DemoApp::Application.initialize!
--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,18 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#myapp-#{Rails.env}"
 
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect if forked
-   end
+
+if ENV['MONGOHQ_URL']
+	MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+	MongoMapper.connect(RAILS_ENV)
+	MongoMapper.database = "demo_app-#{Rails.env}"
+else
+	MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+	MongoMapper.database = "demo_app-#{Rails.env}"
+
+	if defined?(PhusionPassenger)
+		PhusionPassenger.on_event(:starting_worker_process) do |forked|
+			MongoMapper.connection.connect if forked
+		end
+	end	
+  
 end
+
